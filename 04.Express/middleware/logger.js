72811806c63031defa655c5324e73527c495bc6f
gabrmascarenhas <gdmoura412@gmail.com>
2025-05-27 16:00:11 -0300

    feat/logger.js

---
+++ 04.Express/middleware/logger.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
+
+const logsDir = path.join(__dirName, '.../logs');
+const logFile = path.join(logsDir, 'requests.json');
+
+function ensureLogfile() {
+    if(!fs.existsSync(logsDir)){
+        fs.mkdirSync(logsDir);
+    }
+    if(!fs.existsSync(logFile)){
+        fs.writeFileSync(logFile, '[]');
+    }
+}
+
+function logger(request, reponse, next){
+    ensureLogfile();
+    
+    const logData = {
+        date: new Date().toLocaleDateString(),
+        metodo: request.method,
+        url: request.url,
+        body: request.body
+    };
+}
+console.log(`[${logData.date}] [${logData.metodo}] ${logData.url}`);
+
+fs.readFile(logFile, "utf-8", (err, data) => {
+    let logs = [];
+    try{
+        logs = JSON.parse(data || '[]');
+    } catch {
+        logs = [];
+    }
+
+    logs.push(logData);
+    
+    fs.writeFile(logFile, JSON.stringify(logs, null, 2), (err) => {
+        if (err){ console.error("Erro ao salvar log: ", err);}
+    });
+});
+
+export default logger;
