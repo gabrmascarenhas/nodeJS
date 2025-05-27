bdad7a15343f90706ebe16c9da8cc9aa4e7a1465
gabrmascarenhas <gdmoura412@gmail.com>
2025-05-27 16:14:24 -0300

    feat/app.js

--- 04.Express/app.js
+++ 04.Express/app.js
@@ -1,4 +1,22 @@
+
 import express from 'express';
 import userRoutes from "./routes/userRoutes.js";
 import logger from './middleware/logger.js'
+
 const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use(logger);
+
+//Routes
+app.use('/api/users', userRoutes);
+
+//Server
+const host = "localhost";
+const port = 3000;
+
+app.listen(port, () => {
+    console.log("Servidor rodando em https://loccalhost:3300");
+    console.log(`Servidor rodando em: https://${host}:${port}`);
+});
